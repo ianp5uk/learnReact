f4884db209114124fda27be752eb9cb48fae9abb
Ian <ianp5uk@outlook.com>
2023-01-16 21:39:33 +0000

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { Car } from "./components/Car/Car";
+import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
 
 export function App() {
-    function hello(value) {
-        alert("Hello from <App /> " + value);
-    }
-    return (
-        <div>
-            <p>I am the {"<App />"}</p>
-            <Car onCarClick={hello}/>
-        </div>
-    );
+    return <div>
+        <DisplayDifficulty difficulty={"Low"} />
+    </div>;
 }
--- src/components/Car/Car.jsx
+++
@@ -1,6 +0,0 @@
-export function Car(props) {
-    function onClick() {
-        props.onCarClick(2);
-    }
-    return <p onClick={onClick}>I am the {"<Car />"}</p>;
-}

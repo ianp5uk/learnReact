20434aa3d7d64ad047519a8e4934644dd029a454
Ian <ianp5uk@outlook.com>
2023-01-22 17:12:37 +0000

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import s from "./style.module.css";
 import { useState } from "react";
 
 export function App() {
-    const [currentDifficulty, setCurrentDifficulty] = useState("Insane");
+    const [currentDifficulty, setCurrentDifficulty] = useState("");
 
     const onMenuListItemClick = (difficulty) => {
         setCurrentDifficulty(difficulty);
     };
-    return(            
+    return (
         <div>
-            <h1>Select your difficulty</h1>
+            <h1 style={{ textAlign: "center" }}>Select your difficulty</h1>
             <div className={s.workspace}>
-                <MenuList onItemClick={onMenuListItemClick} />
+                <MenuList
+                    difficulty={currentDifficulty}
+                    onItemClick={onMenuListItemClick}
+                />
                 <DisplayDifficulty difficulty={currentDifficulty} />
             </div>
-        </div>)
+        </div>
+    );
 }
